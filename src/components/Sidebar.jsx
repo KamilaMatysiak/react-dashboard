f3b2d628dc9bf88d3d2584d80d0730db6b1cf77d
xkamikoo <58092037+xkamikoo@users.noreply.github.com>
2022-08-23 14:14:52 +0200

    Add Navbar component + basic sidebar logic

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,15 +10,22 @@ import { useStateContext } from '../contexts/ContextProvider';
 const Sidebar = () => {
 
 
-  const {activeMenu, setActiveMenu} = useStateContext();
-  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
+  const {activeMenu, setActiveMenu, screenSize} = useStateContext();
+
+  const handleCloseSidebar = () => {
+    if(activeMenu && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  }
+
+  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-gray-400 dark:text-white text-md m-2';
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-gray-700 text-md m-2 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray';
 
   return (
     <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
       {activeMenu && (<>
         <div className='flex justify-between items-center'>
-          <Link to="/" onClick={() => {setActiveMenu(false)}} className='itemst-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900'>
+          <Link to="/" onClick={() => {handleCloseSidebar()}} className='itemst-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900'>
             <SiShopware /> Shoppy
           </Link>
           <TooltipComponent content="Menu" position='BottomCenter'>
@@ -31,9 +38,13 @@ const Sidebar = () => {
           <div key={item.title}>
             <p className='text-gray-400 m-3 mt-4 uppercase'>{item.title}</p>
             {item.links.map((link) => (
-              <NavLink to={`/${link.name}`} key={link.name} onClick={() => {}}
-              className={({ isActive }) => isActive ? activeLink : normalLink}>
-                {link.icon}
+              <NavLink 
+                to={`/${link.name}`} 
+                key={link.name} 
+                onClick={handleCloseSidebar()}
+                className={({ isActive }) => isActive ? activeLink : normalLink}>
+
+                  {link.icon}
                 <span className='capitalize'>{link.name}</span>
               </NavLink>
             ))}
