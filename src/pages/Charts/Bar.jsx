e519372bca863ad37b8db51b9fd9736605e64aa9
xkamikoo <58092037+xkamikoo@users.noreply.github.com>
2022-08-24 15:56:27 +0200

    Add BarChart and PieChart to Chart pages

--- src/pages/Charts/Bar.jsx
+++ src/pages/Charts/Bar.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import { ChartsHeader} from '../../components'
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, Tooltip, ColumnSeries, DataLabel } from '@syncfusion/ej2-react-charts';
+
+import { barCustomSeries, barPrimaryYAxis, barPrimaryXAxis } from '../../data/dummy'
+import { useStateContext } from '../../contexts/ContextProvider'
 
 const Bar = () => {
+  const { currentMode } = useStateContext();
+
   return (
-    <div>Bar</div>
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+      <ChartsHeader category="Chart" title="Bar Chart"/>
+      <div className='w-full'>
+      <div>
+        <ChartComponent
+          id="bar-chart"
+          height='420px'
+          primaryYAxis={barPrimaryYAxis}
+          primaryXAxis={barPrimaryXAxis}
+          chartArea={{border: {width: 0}}}
+          tooltip={{enable: true}}
+          background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+          >
+          <Inject services={[Legend, Category, Tooltip, ColumnSeries, DataLabel]}/>
+          <SeriesCollectionDirective>
+            {barCustomSeries.map((item, index) => (
+              <SeriesDirective key={index}{...item}/>
+            ))}
+          </SeriesCollectionDirective>
+        </ChartComponent>
+      </div>
+      </div>
+    </div>
   )
 }
 
