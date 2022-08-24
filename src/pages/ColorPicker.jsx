31671ff2bad4fb794e4e7c1bdfd878c8231c1d67
xkamikoo <58092037+xkamikoo@users.noreply.github.com>
2022-08-24 12:57:10 +0200

    Add ColorPicker Module

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import { ColorPickerComponent} from '@syncfusion/ej2-react-inputs'
+import { Header } from '../components'
+
+const change = (args) => {
+  document.getElementById("preview").style.backgroundColor = args.currentValue.hex;
+}
 
 const ColorPicker = () => {
   return (
-    <div>ColorPicker</div>
+    <div className='m-2 md:m-10 mt-24 md:p-10 p-2 bg-white rounded-3xl'>
+      <Header category="App" title="Color Picker"/>
+      <div className='text-center'>
+        <div id="preview"/>
+        <div className='flex justify-center items-center gap-20 flex-wrap'>
+          <div>
+            <p className='text-2xl mt-2 mb-4 font-semibold'>Inline Pallete</p>
+            <ColorPickerComponent 
+              id="inline-pallete" 
+              mode="Palette" 
+              modeSwitcher={false}
+              showButtons={false}
+              inline
+              change={change} />
+          </div>
+          <div>
+            <p className='text-2xl mt-2 mb-4 font-semibold'>Inline Picker</p>
+            <ColorPickerComponent 
+              id="inline-picker" 
+              mode="Picker" 
+              modeSwitcher={false}
+              showButtons={false}
+              inline 
+              change={change}/>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
